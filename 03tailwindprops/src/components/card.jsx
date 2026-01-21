b85217c2b9b8bc450b4b61cda449db2ee1c106e5
kareenapatidar329 <kareenapatidar329@gmail.com>
2026-01-21 23:49:41 +0530

    Add 04bgChanger

---
+++ 03tailwindprops/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card({username}) {
+    //console.log("props" , props);
+    console.log(props.username);
+    return (
+        <div>Card</div>
+    )
+}
+
+export default Card
